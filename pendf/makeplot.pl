e2a45b541fc703633754fcf5683536ce486326f6
Shin OKUMURA <s.okumura@iaea.org>
2020-07-24 16:49:24 +0200

    makeplot.plwq

--- pendf/makeplot.pl
+++ pendf/makeplot.pl
@@ -76,6 +76,8 @@ foreach my $files (@sorted){
     print PLT "set output '$eps'\n";
     print PLT "set format x '10^{%L}'\nset xtics 10\nset log x\n";
     print PLT "set xlabel 'Incident photon energy [eV]'\nset ylabel 'Cross section [b]'\n";
+    print PLT "set format y '10^{%L}'\nset ytics 10\nset key bottom\nset log y\n replot\n";
+    
     if ($nucl =~ /0128|0425|0625|0725|0825/){
          print PLT "plot '$mf3' u 1:2 ti 'MF3 MT3' w l lw 2 lc rgb 'black',";
     }
@@ -86,28 +88,41 @@ foreach my $files (@sorted){
     # Find non LAW=0 data from MF6 file and write them to another file with an extention .resid
     while(<MF6>){
 	my $line           = $_;
-	if(/ZAP/){ $zap = substr($line,15,13); $zap = trim($zap);}
+	if(/ZAP/){ $zap         = substr($line,15,13);
+		   $zap         = trim($zap);
+		   my $elem     = zaptoelem($zap);
+		   my $resi_z   = int($zap/1000);
+		   my $resi_a   = $zap-(1000*$resi_z);
+		   if($elem =~ /^[a-p]{1}/ ){$residual = $elem;}
+		   else{$residual    = $elem ."-". $resi_a};
+		   #print "$zap, $elem,$resi_z,$resi_a,$residual\n";
+	}
 	if(/LAW/){ $law = substr($line,15,13); $law = trim($law);}
 	if(/NP/) { $np  = substr($line,15,13); $np  = trim($np) ; $npl = $. ;
-		   
 		# select $law == 1 or 0 in line 93 and 100.
-		if($law == 0){
+		# if($law == 0){
 		    $index ++;
 		    print OUT "\n\n\n#     $index\n#     ZAP    $zap\n#     LAW    $law\n#     NP    $np\n";
 		    # Write gnuplot input for MF6 part
 		    print PLT "'$out' i $index u 1:4 ti '$zap' w l,";
-		}
+	        # }
 	}
-	if($line =~ /^\s[0-9]/ && $law == 0 && $. > $npl+1 && $. < $npl+1+$np ) {print OUT "$line";}
+	if($line =~ /^\s[0-9]/ && $law >= 0 && $. > $npl+1 && $. < $npl+1+$np ) {print OUT "$line";}
     }
+    
     # Write gnuplot input with y axis in log scale
-    print PLT "\n\nset output '$epsl'\n";
-    print PLT "set format y '10^{%L}'\nset ytics 10\nset key bottom\nset log y\n replot\n";
+    
+    # print PLT "\n\nset output '$epsl'\n";
+    # print PLT "set format y '10^{%L}'\nset ytics 10\nset key bottom\nset log y\n replot\n";
+    
     # Run gnuplot
     my $return_value = `gnuplot "$plot"`;
     # Write tex file
-    print TEX "\\begin{figure}\n \\includegraphics[width=\\linewidth]{$eps}\n  \\caption{$nucl}\n\\end{figure}\n";
-    print TEX "\\begin{figure}\n \\includegraphics[width=\\linewidth]{$epsl}\n \\caption{$nucl}\n\\end{figure}\n\\newpage \\clearpage\n\n";
+    #    print TEX "\\begin{figure}\n \\includegraphics[width=\\linewidth]{$eps}\n  \\caption{$residual}\n\\end{figure}\n";
+    
+    print TEX "\\begin{figure}\n \\includegraphics[width=\\linewidth]{$eps}\n  \\caption{$residual}\n\\end{figure}\n";
+    print TEX "\\begin{figure}\n \\includegraphics[width=\\linewidth]{$epsl}\n \\caption{$residual}\n\\end{figure}\n\\newpage \\clearpage\n\n";
+    
 #    }
 }
 # Run tex compile
@@ -123,5 +138,36 @@ sub trim {
 	my $val = shift;
 	$val =~ s/^ *(.*?) *$/$1/;
 	return $val;
+}
+
+
+sub zaptoelem {
+    my $zap = shift;
+    my $elem_name = "";
+    if    ($zap == "0")   {$elem_name = "g";}
+    elsif ($zap == "1")   {$elem_name = "n";}
+    elsif ($zap == "1001"){$elem_name = "p";}
+    elsif ($zap == "1002"){$elem_name = "d";}
+    elsif ($zap == "2004"){$elem_name = "alpha";}
+    else{
+	my $resi_z = int($zap/1000);
+	my @elem_list=("", 
+		       "H" , "He", "Li", "Be", "B" , "C" , "N" , "O" , "F" , "Ne",
+		       "Na", "Mg", "Al", "Si", "P" , "S" , "Cl", "Ar", "K" , "Ca",
+		       "Sc", "Ti", "V" , "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+		       "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y" , "Zr",
+		       "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn",
+		       "Sb", "Te", "I" , "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd",
+		       "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+		       "Lu", "Hf", "Ta", "W" , "Re", "Os", "Ir", "Pt", "Au", "Hg",
+		       "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th",
+		       "Pa", "U" , "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
+		       "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds",
+		       "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og", "Uue","Ubn",
+		       "Ubu","Ubb","Ubt","Ubq","Ubp","Ubh","Ubs","Ubo","Ube","Utn",
+		       "Utu","Utb","Utt","Utq","Utp","Uth","Uts","Uto","Ute","Uqn",);
+	 $elem_name = $elem_list[$resi_z];
+    }
+  return $elem_name;
 }
 
